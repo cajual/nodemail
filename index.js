5645c44d3bc69189eaf3a1b1ecd58fc1b6f97d62
Ryan Gibbons <sotf.caj@gmail.com>
2020-05-17 12:05:20 -0400

    Updated environment variables.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-const mailer = require('nodemailer')
+require('dotenv').config()
+var nodemailer = require('nodemailer');
+var smtpTransport = require('nodemailer-smtp-transport');
+
+var transporter = nodemailer.createTransport(smtpTransport({
+  service: 'gmail',
+  host: 'smtp.gmail.com',
+  auth: {
+    user: process.env.GMAIL_NAME,
+    pass: process.env.GMAIL_PASS
+  }
+}));
+
+var mailOptions = {
+  from: process.env.GMAIL_NAME,
+  to: process.env.SEND_TO,
+  subject: 'Sending Email using Node.js[nodemailer]',
+  text: 'That was easy!'
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+});
+
+console.log('Test')
